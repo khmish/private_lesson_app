ccd4378fd097667a3bb2fc126da7a8da81f5b46b
x1337-sa <fahad.h11@outlook.com>
2021-10-19 08:45:46 +0300

    add permissions

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:private_lesson_app/permissions.dart';
 import 'package:private_lesson_app/signup.dart';
 import 'package:private_lesson_app/city.dart';
 import 'login.dart';
 
-
 void main() {
-  runApp(
-    MaterialApp(
-      //home: SignupWidget(),
-      // home: LoginPageWidget(),
-      home: CityWidget(),
-    )
-  );
+  runApp(MaterialApp(
+    //home: SignupWidget(),
+    // home: LoginPageWidget(),
+    home: permissions(),
+  ));
 }
-

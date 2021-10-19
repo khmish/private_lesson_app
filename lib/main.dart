4c4e919799a7059f99c1124d65791637eaeca89b
khmish <30586743+khmish@users.noreply.github.com>
2021-10-19 17:04:15 +0300

    add main search

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:private_lesson_app/signup.dart';
-import 'package:private_lesson_app/city.dart';
-import 'login.dart';
+import 'package:private_lesson_app/pages/main_search.dart';
+import 'package:private_lesson_app/pages/signup.dart';
+import 'package:private_lesson_app/pages/city.dart';
+import 'pages/login.dart';
 
 
 void main() {
   runApp(
     MaterialApp(
-      home: SignupWidget(),
+      home: SearchWidget(),
+      // home: SignupWidget(),
       // home: LoginPageWidget(),
       // home: CityWidget(),
     )

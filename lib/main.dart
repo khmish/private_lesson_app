943c60310d6e973060e7faab91309e6624077988
meshalalmufleh <meshalmufleh20@gmail.com>
2021-11-17 23:24:18 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:private_lesson_app/pages/home.dart';
 import 'package:private_lesson_app/pages/main_search.dart';
 import 'package:private_lesson_app/pages/myorder_page.dart';
-import 'package:private_lesson_app/pages/signup.dart';
+import 'package:private_lesson_app/pages/register_login/signup.dart';
 import 'package:private_lesson_app/pages/side_menu.dart';
-import 'package:private_lesson_app/pages/signup_teacher.dart';
+import 'package:private_lesson_app/pages/register_login/signup_teacher.dart';
 import 'package:private_lesson_app/pages/teacher_profile.dart';
 import 'package:private_lesson_app/pages/admin/admin_control.dart';
 import 'package:private_lesson_app/pages/myprofile.dart';
 
-import 'pages/login.dart';
+import 'pages/register_login/login.dart';
 
 void main() {
   runApp(MaterialApp(

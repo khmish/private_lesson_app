af44d978ce46ce8366b851e11c6dcfeba96633a5
meshalalmufleh <meshalmufleh20@gmail.com>
2021-11-24 17:26:33 +0300

    Update myprofile.dart

--- lib/pages/myprofile.dart
+++ lib/pages/myprofile.dart
@@ -36,91 +36,105 @@ class _MyprofileScreenState extends State<MyprofileScreen> {
 // class MyprofileScreen extends StatelessWidget {
 //   MyprofileScreen({Key? key}) : super(key: key);
 
-  
-
   @override
   Widget build(BuildContext context) {
-    
     // Use the Todo to create the UI.
     return isLoading == true
-          ? Center(
-              child: LinearProgressIndicator(),
-            )
-          :Scaffold(
-        backgroundColor: (colorContainerBox),
-        // appBar: AppBar(
-        //   title: Text('test profile'),
-        // ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width > 1000
-                      ? MediaQuery.of(context).size.width * 0.6
-                      : MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                      color: colorBackGround,
-                      image: DecorationImage(
-                          image: NetworkImage("add you image URL here "),
-                          fit: BoxFit.cover)),
-                  child: Container(
-                    width: double.infinity,
-                    height: 160,
-                    child: Container(
-                      alignment: Alignment(0.0, 2.5),
-                      child: CircleAvatar(
-                        backgroundImage:
-                            NetworkImage("https://picsum.photos/seed/305/600"
+        ? Center(
+            child: LinearProgressIndicator(),
+          )
+        : Scaffold(
+            backgroundColor: (colorContainerBox),
+            // appBar: AppBar(
+            //   title: Text('test profile'),
+            // ),
+            body: SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width > 1000
+                          ? MediaQuery.of(context).size.width * 0.6
+                          : MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          color: colorBackGround,
+                          image: DecorationImage(
+                              image: NetworkImage("add you image URL here "),
+                              fit: BoxFit.cover)),
+                      child: Container(
+                        width: double.infinity,
+                        height: 160,
+                        child: Container(
+                          alignment: Alignment(0.0, 2.5),
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "https://picsum.photos/seed/305/600"
                                 // "https://i.ibb.co/JzdX185/profile-male.png"
                                 ),
-                        radius: 60.0,
+                            radius: 60.0,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                    SizedBox(
+                      height: 60,
+                    ),
+                    Text(
+                      'Name: ${myuser.name}',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          fontSize: 25.0,
+                          color: colorMainText,
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Email: ${myuser.email}',
+                      style: TextStyle(
+                          fontSize: 18.0,
+                          color: colorMainText,
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.w300),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Gender: ${myuser.gender}',
+                      style: TextStyle(
+                          fontSize: 18.0,
+                          color: colorMainText,
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.w300),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Phone number: ${myuser.phone}',
+                      style: TextStyle(
+                          fontSize: 18.0,
+                          color: colorMainText,
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.w300),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'City: ${myuser.city}',
+                      style: TextStyle(
+                          fontSize: 18.0,
+                          color: colorMainText,
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  height: 60,
-                ),
-                Text(
-                  // "name: ",
-                  'Name: ${myuser.name}',
-                  style: TextStyle(
-                      fontSize: 25.0,
-                      color: colorMainText,
-                      letterSpacing: 2.0,
-                      fontWeight: FontWeight.w400),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  // 'Email*********',
-                  'Email: ${myuser.email}',
-                  style: TextStyle(
-                      fontSize: 18.0,
-                      color: colorMainText,
-                      letterSpacing: 2.0,
-                      fontWeight: FontWeight.w300),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-              ],
-            ),
-          ),
-        ));
+              ),
+            ));
   }
-}
-
-class _isLoading {
 }

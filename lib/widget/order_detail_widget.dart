d60fe0bec8e8ac37e3ea9f8aea9f8fab908805af
x1337-sa <fahad.h11@outlook.com>
2021-11-29 10:38:59 +0300

    Update order_detail_widget.dart

--- lib/widget/order_detail_widget.dart
+++ lib/widget/order_detail_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:private_lesson_app/constants/size_const.dart';
 import 'package:private_lesson_app/models/lesson.dart';
 
 class OrderDetail extends StatefulWidget {
@@ -12,7 +13,58 @@ class OrderDetail extends StatefulWidget {
 class _OrderDetailState extends State<OrderDetail> {
   @override
   Widget build(BuildContext context) {
-    widget.lesson.student!.name;
-    return Container();
+    var studentname = widget.lesson.student!.name;
+    var teachername = widget.lesson.teacher!.name;
+    var subject = widget.lesson.subject!.name;
+    var state = widget.lesson.state!;
+
+
+    return Scaffold(
+      appBar: AppBar(
+          title:Text("Order Details"),
+        backgroundColor: (colorContainerBox),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+
+        child: Card(
+            elevation: 4.0,
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text("Details"),
+                  // subtitle: Text("Detailssss"),
+                  // trailing: Icon(Icons.favorite_outline),
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      Text("Student Name: " + studentname!),
+                      Text("Teacher Name: " + teachername!),
+                      Text("Subject: " + subject!),
+                      Text("State: " + state!),
+
+                    ],
+                  ),
+                ),
+                ButtonBar(
+                  children: [
+                    // TextButton(
+                    //   child: const Text('Change'),
+                    //   onPressed: () {/* ... */},
+                    // ),
+                    TextButton(
+                      child: const Text('Cancel'),
+                      onPressed: () {
+                        /* ... */
+                      },
+                    )
+                  ],
+                ),
+              ],
+            )),
+      ),
+    );
   }
 }
